6bb70089dad11319772163f2f07485814f4e3881
fleurret <90931602+fleurret@users.noreply.github.com>
2023-06-15 12:17:45 -0400

    fixed

---
+++ helpers/baseline.m
@@ -0,0 +1,17 @@
+function b = baseline(u,parname)
+
+E = u.Session.Events;
+V = u.UserData.(parname).V;
+M = u.UserData.(parname).M;
+dV = u.UserData.(parname).vals;
+
+% find non-AM
+par.referencevalue = min(V);
+refInd = V == par.referencevalue;
+
+b = nan(1);
+
+ind = dV(1) == V;
+data = [M(refInd); M(ind)]';
+tind = [false(1,sum(refInd)) true(1,sum(ind))];
+b = epa.metric.baseline_activity(data,tind);
